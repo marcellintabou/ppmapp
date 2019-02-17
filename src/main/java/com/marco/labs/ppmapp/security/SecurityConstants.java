86b962dc544fbfaba1ecf97a948371d853fbb3c5
Marcellin <marcellintabou@gmail.com>
2019-02-17 11:13:01 +0100

    some refactoring

--- src/main/java/com/marco/labs/ppmapp/security/SecurityConstants.java
+++ src/main/java/com/marco/labs/ppmapp/security/SecurityConstants.java
@@ -9,7 +9,7 @@ public class SecurityConstants {
 	public static final String HEADER_STRING = "Authorization";
  
 	
-	public static final long EXPIRATION_TIME = 3000_000; // 5minutes
+	public static final long EXPIRATION_TIME = 3000000; // 50minutes
  
 	
 	
